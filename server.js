9539c384579bf0ba7a428dd9de3b4be76d18aeb0
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-09-05 21:01:10 +1200

    mongoose not working need to create database

--- server.js
+++ server.js
@@ -7,12 +7,23 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const config = require('./config.json');
 
-mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@sophiecluster-lhxyp.mongodb.net/shop?retryWrites=true&w=majority`, {useNewUrlParser: true});
+mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@sophiecluster-lhxyp.mongodb.net/work?retryWrites=true&w=majority`, {useNewUrlParser: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log(`we're connected!`);
 });
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+
+app.use(function(req, res, next){
+    console.log(`${req.method} request for ${req.url}`);
+    next();
+});
+
 
 app.get('/',function(req,res){
     res.send('Welcome! This is our portfolio.');
@@ -50,6 +61,43 @@ app.get('/update',function(req,res){
     res.send('Welcome! This is our Update endpoint.');
 });
 
+
+app.post('/users',function(req,res){
+  User.findOne({ username:req.body.username}, function(err, result){
+    if (result) {
+        res.send('Sorry, this is already existed');
+    } else {
+      const hash = bcrypt.hashSync(req.body.password);
+      const user = new User({
+        _id: new mongoose.Types.ObjectId(),
+        username: req.body.username,
+        email: req.body.email,
+        password: hash
+      });
+      user.save().then(result => {
+        res.send(result)
+      }).catch(err => res.send(err))
+    }
+  })
+})
+
+app.post('/getUser', function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ username: username}, function(err, result){
+    if (checkUser) {
+        if (bcrypt.compareSync(password,checkUser.password)) {
+          console.log('password matched');
+          res.send(checkUser)
+        } else {
+          console.log('password does not matched');
+          res.send('Invalid password');
+        }
+    } else {
+      res.send('Invalid user')
+    }
+  })
+})
 
 // Listen to port 3000
 app.listen(port, () => {
