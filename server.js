61235f4cae0f57adbd68d0d9701ecd8a555ae269
Larissa <cloudcity101@gmail.com>
2019-09-05 20:00:17 +1200

    endpint broken

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8888;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const config = require('./config.json');
+const Work = require('./models/work');
 
 mongoose.connect(`mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@sophiecluster-lhxyp.mongodb.net/shop?retryWrites=true&w=majority`, {useNewUrlParser: true});
 var db = mongoose.connection;
@@ -13,6 +14,10 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log(`we're connected!`);
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
 
 app.get('/',function(req,res){
     res.send('Welcome! This is our portfolio.');
@@ -29,27 +34,26 @@ app.delete('/view/:id',function(req,res){
     res.send('Welcome! This is delete endpoint.');
 });
 
-app.get('/add',function(req,res){
+app.get('/add', function(req,res){
     // res.send('Welcome! This is our Create endpoint');
   const workItem = new Work({
-    id: mongoose.Schema.Types.ObjectId(),
-    workName: Stringreq.body.workName,
-    workAuthor: Stringreq.body.workAuthor,
-    workImg: Stringreq.body.workImg,
-    authorURL: Stringreq.body.authorURL
+    id: mongoose.Types.ObjectId(),
+    workName: String.req.body.workName,
+    workAuthor: String.req.body.workAuthor,
+    workImg: String.req.body.workImg,
+    authorURL: String.req.body.authorURL
   });
 
-  workItem.save().then(result => {
-    res.send(result);
-  }).catch(err => res.send(err));
+  console.log(workItem);
+  // workItem.save().then(result => {
+  //   res.send(result);
+  // }).catch(err => res.send(err));
 });
 
 app.get('/update',function(req,res){
     res.send('Welcome! This is our Update endpoint.');
 });
 
-
-// Listen to port 3000
 app.listen(port, () => {
     console.log(`application is running on port ${port}`);
 });
